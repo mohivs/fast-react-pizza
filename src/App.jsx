9c0b6d5d27b73a0801b36e5529ad3fd7829a978b
MohiVs <mohisls2012@gmail.com>
2024-07-06 14:06:49 +0330

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/menu", element: <Menu /> },
-  { path: "/cart", element: <Cart /> },
-  { path: "/order/new", element: <CreateOrder /> },
-  { path: "/order/:orderId", element: <Order /> },
+  {
+    element: <AppLayout />,
+
+    children: [
+      { path: "/", element: <Home /> },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+      { path: "/cart", element: <Cart /> },
+      { path: "/order/new", element: <CreateOrder /> },
+      { path: "/order/:orderId", element: <Order /> },
+    ],
+  },
 ]);
 
 function App() {
-  return <RouterProvider router={router}></RouterProvider>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast react pizza</Link>
+      <p>mohsen</p>
+    </header>
+  );
+}
+
+export default Header;
