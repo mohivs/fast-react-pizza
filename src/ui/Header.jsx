504b96249a074a769129026069bae87a8d57b24b
mohsen <mohisls2012@gmail.com>
2024-07-06 21:18:27 +0330

    asd

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
   return (
     <header>
       <Link to="/">Fast react pizza</Link>
+      <SearchOrder />
       <p>mohsen</p>
     </header>
   );
