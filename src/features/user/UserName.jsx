a16d17458c32331999cfd4f28154b5dfd210a91a
mohsen <mohisls2012@gmail.com>
2024-07-08 21:12:19 +0330

    sd

---
+++ src/features/user/UserName.jsx
@@ -0,0 +1,5 @@
+function UserName() {
+  return <div className="hidden text-sm font-semibold md:block">mohsen</div>;
+}
+
+export default UserName;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import UserName from "../features/user/UserName";
 
 function Header() {
   return (
-    <header>
-      <Link to="/">Fast react pizza</Link>
+    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase">
+      <Link to="/" className="tracking-widest">
+        Fast react pizza
+      </Link>
       <SearchOrder />
-      <p>mohsen</p>
+      <UserName />
     </header>
   );
 }
